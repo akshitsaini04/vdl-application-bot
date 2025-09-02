c211d28048ea7c3746e1860917ccbcbbe0c58927
Akshit Saini <89911555+akshitsaini04@users.noreply.github.com>
2025-09-03 04:31:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,24 @@
 <?php
-// Get secrets from Vercel Environment Variables
-define('APPLICATION_ID', $_ENV['APPLICATION_ID']);
-define('BOT_TOKEN', $_ENV['BOT_TOKEN']);
-define('PUBLIC_KEY', $_ENV['PUBLIC_KEY']);
-define('ADMIN_CHANNEL_ID', $_ENV['ADMIN_CHANNEL_ID']);
-define('PUBLIC_CHANNEL_ID', $_ENV['PUBLIC_CHANNEL_ID']);
+
+// Find this on the "General Information" page of your Discord Application
+define('APPLICATION_ID', 'YOUR_APPLICATION_ID_HERE');
+
+// --- NEW: Add these two lines for the login system ---
+// Find this on the "OAuth2 > General" page
+define('CLIENT_ID', 'YOUR_CLIENT_ID_HERE'); 
+define('CLIENT_SECRET', 'YOUR_CLIENT_SECRET_HERE');
+
+// Find this on the "Bot" page
+define('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE');
+
+// Find this on the "General Information" page
+define('PUBLIC_KEY', 'YOUR_APPLICATION_PUBLIC_KEY_HERE');
+
+// Right-click your private admin channel and "Copy Channel ID"
+define('ADMIN_CHANNEL_ID', 'YOUR_ADMIN_CHANNEL_ID_HERE');
+
+// Right-click your public results channel and "Copy Channel ID"
+define('PUBLIC_CHANNEL_ID', 'YOUR_PUBLIC_CHANNEL_ID_HERE');
+
 ?>
+
